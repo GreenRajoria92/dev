90d73a7792f0c79ebf96bd0b1762d9c2a6b79eae
Green Rajoria <147193191+GreenRajoria92@users.noreply.github.com>
2024-05-29 07:28:44 +0000

    main

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -1,20 +1,20 @@
 export const educations = [
   {
     id: 1,
-    title: "Bachelor Degree",
-    duration: "2020 - Present",
-    institution: "National University of Bangladesh",
+    title: "B.Tech",
+    duration: "2012-2016",
+    institution: "National Institute of technology, Warangal",
   },
   {
     id: 2,
-    title: "Higher Secondary Certificate",
-    duration: "2018 - 2020",
-    institution: "Noakhali Islamia Kamil Madrasah",
+    title: "Higher Secondary ",
+    duration: "2008-2010",
+    institution: "St. John's Dio. Sr. Sec. School, Ajmer",
   },
   {
     id: 3,
-    title: "Secondary School Certificate",
-    duration: "2008 - 2018",
-    institution: "Baitus Saif Islamia Madrasah",
+    title: "Secondary School ",
+    duration: "2007-2008",
+    institution: "St.Thomas Sr. Sec. School, Ajmer",
   }
 ]
